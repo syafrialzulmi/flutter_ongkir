6662fa60db9c5695b5c8d84686dad8dc3450ed39
Syafrial Zulmi <syafrialzulmi@gmail.com>
2022-03-19 11:51:28 +0700

    ongkos kirim apps

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,20 +1,78 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+// import 'package:dio/dio.dart';
+import 'package:http/http.dart' as http;
+
+import '../../../data/models/ongkir_model.dart';
 
 class HomeController extends GetxController {
-  //TODO: Implement HomeController
+  TextEditingController beratC = TextEditingController();
 
-  final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
+  List<Ongkir> ongkosKirim = [];
+
+  RxBool isLoading = false.obs;
+
+  RxString provAsalId = "0".obs;
+  RxString cityAsalId = "0".obs;
+  RxString provTujuanId = "0".obs;
+  RxString cityTujuanId = "0".obs;
+
+  RxString codeKurir = "".obs;
+
+  void cekOngkir() async {
+    if (provAsalId != "0" &&
+        cityAsalId != "0" &&
+        provTujuanId != "0" &&
+        cityTujuanId != "0" &&
+        beratC.text != "" &&
+        codeKurir != "") {
+      try {
+        isLoading.value = true;
+        var response = await http
+            .post(Uri.parse("https://api.rajaongkir.com/starter/cost"), body: {
+          "origin": cityAsalId.value,
+          "destination": cityTujuanId.value,
+          "weight": beratC.text,
+          "courier": codeKurir.value,
+        }, headers: {
+          "key": "372146ea941c8f928014866aba042c0f",
+          "content-type": "application/x-www-form-urlencoded"
+        });
+
+        isLoading.value = false;
+        List ongkir = json.decode(response.body)['rajaongkir']['results'][0]
+            ['costs'] as List;
+        ongkosKirim = Ongkir.fromJsonList(ongkir);
+
+        Get.defaultDialog(
+          title: "ONGKOS KIRIM",
+          content: Column(
+            children: ongkosKirim
+                .map((e) => ListTile(
+                      title: Text('${e.service!.toUpperCase()}'),
+                      subtitle: Text('Rp. ${e.cost![0].value}'),
+                    ))
+                .toList(),
+          ),
+        );
+
+        // datanya.forEach((element) {
+        //   print(element.toJson());
+        // });
+      } catch (e) {
+        print(e);
+        Get.defaultDialog(
+          title: "Terjadi Kesalahan",
+          middleText: "Tidak dapat mengecek ongkos kirim",
+        );
+      }
+    } else {
+      Get.defaultDialog(
+        title: "Terjadi Kesalahan",
+        middleText: "Data input kurang lengkap",
+      );
+    }
   }
-
-  @override
-  void onReady() {
-    super.onReady();
-  }
-
-  @override
-  void onClose() {}
-  void increment() => count.value++;
 }
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,22 +1,178 @@
+import 'package:dropdown_search/dropdown_search.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 
 import '../controllers/home_controller.dart';
+import '../../../data/models/province_model.dart';
+import '../../../data/models/city_model.dart';
 
 class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('HomeView'),
+        title: Text('CEK ONGKOS KIRIM'),
         centerTitle: true,
       ),
-      body: Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
-        ),
+      body: ListView(
+        padding: EdgeInsets.all(20),
+        children: [
+          DropdownSearch<Province>(
+            showSearchBox: true,
+            popupItemBuilder: (context, item, isSelected) => ListTile(
+              title: Text("${item.province}"),
+            ),
+            dropdownSearchDecoration: InputDecoration(
+              labelText: "Provinsi Asal",
+              contentPadding: EdgeInsets.symmetric(vertical: 5, horizontal: 15),
+              border: OutlineInputBorder(),
+            ),
+            onFind: (text) async {
+              var response = await Dio().get(
+                "https://api.rajaongkir.com/starter/province",
+                queryParameters: {"key": "372146ea941c8f928014866aba042c0f"},
+              );
+
+              return Province.fromJsonList(
+                  response.data["rajaongkir"]["results"]);
+            },
+            onChanged: (value) =>
+                controller.provAsalId.value = value?.provinceId ?? "0",
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          DropdownSearch<City>(
+            showSearchBox: true,
+            popupItemBuilder: (context, item, isSelected) => ListTile(
+              title: Text("${item.type} ${item.cityName}"),
+            ),
+            dropdownSearchDecoration: InputDecoration(
+              labelText: "Kota/ Kabupaten Asal",
+              contentPadding: EdgeInsets.symmetric(vertical: 5, horizontal: 15),
+              border: OutlineInputBorder(),
+            ),
+            onFind: (text) async {
+              var response = await Dio().get(
+                "https://api.rajaongkir.com/starter/city?province=${controller.provAsalId}",
+                queryParameters: {"key": "372146ea941c8f928014866aba042c0f"},
+              );
+
+              return City.fromJsonList(response.data["rajaongkir"]["results"]);
+            },
+            onChanged: (value) =>
+                controller.cityAsalId.value = value?.cityId ?? "0",
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          DropdownSearch<Province>(
+            showSearchBox: true,
+            popupItemBuilder: (context, item, isSelected) => ListTile(
+              title: Text("${item.province}"),
+            ),
+            dropdownSearchDecoration: InputDecoration(
+              labelText: "Provinsi Tujuan",
+              contentPadding: EdgeInsets.symmetric(vertical: 5, horizontal: 15),
+              border: OutlineInputBorder(),
+            ),
+            onFind: (text) async {
+              var response = await Dio().get(
+                "https://api.rajaongkir.com/starter/province",
+                queryParameters: {"key": "372146ea941c8f928014866aba042c0f"},
+              );
+
+              return Province.fromJsonList(
+                  response.data["rajaongkir"]["results"]);
+            },
+            onChanged: (value) =>
+                controller.provTujuanId.value = value?.provinceId ?? "0",
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          DropdownSearch<City>(
+            showSearchBox: true,
+            popupItemBuilder: (context, item, isSelected) => ListTile(
+              title: Text("${item.type} ${item.cityName}"),
+            ),
+            dropdownSearchDecoration: InputDecoration(
+              labelText: "Kota/ Kabupaten Tujuan",
+              contentPadding: EdgeInsets.symmetric(vertical: 5, horizontal: 15),
+              border: OutlineInputBorder(),
+            ),
+            onFind: (text) async {
+              var response = await Dio().get(
+                "https://api.rajaongkir.com/starter/city?province=${controller.provTujuanId}",
+                queryParameters: {"key": "372146ea941c8f928014866aba042c0f"},
+              );
+
+              return City.fromJsonList(response.data["rajaongkir"]["results"]);
+            },
+            onChanged: (value) =>
+                controller.cityTujuanId.value = value?.cityId ?? "0",
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          TextField(
+            controller: controller.beratC,
+            autocorrect: false,
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              labelText: "Berat (gram)",
+              contentPadding:
+                  EdgeInsets.symmetric(vertical: 25, horizontal: 15),
+              border: OutlineInputBorder(),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          DropdownSearch<Map<String, dynamic>>(
+            items: [
+              {
+                "code": "jne",
+                "name": "JNE",
+              },
+              {
+                "code": "pos",
+                "name": "POS Indonesia",
+              },
+              {
+                "code": "tiki",
+                "name": "TIKI",
+              }
+            ],
+            showSearchBox: true,
+            popupItemBuilder: (context, item, isSelected) => ListTile(
+              title: Text("${item['name']}"),
+            ),
+            dropdownSearchDecoration: InputDecoration(
+              labelText: "Pilih Kurir",
+              contentPadding: EdgeInsets.symmetric(vertical: 5, horizontal: 15),
+              border: OutlineInputBorder(),
+            ),
+            dropdownBuilder: (context, selectedItem) =>
+                Text("${selectedItem?['name'] ?? "Pilih Kurir"}"),
+            onChanged: (value) =>
+                controller.codeKurir.value = value?['code'] ?? "",
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Obx(() => ElevatedButton(
+              onPressed: () {
+                if (controller.isLoading.isFalse) {
+                  controller.cekOngkir();
+                }
+              },
+              child: Text(controller.isLoading.isFalse
+                  ? "CEK ONGKOS KIRIM"
+                  : "LOADING...")))
+        ],
       ),
     );
   }
